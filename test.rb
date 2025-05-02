a1dedb65adfcf906bff4db72a7bf36e3266c2b8a
esopp <esoppenheim@gmail.com>
2025-05-02 10:56:50 -0400

    calculate 1d variance with a cached result, while adding one element to the set

--- test.rb
+++ test.rb
@@ -13,5 +13,14 @@ class TestMath < Test::Unit::TestCase
   def test_var1D
     assert_equal 4.67, var1D([5, 6, 10]).round(2)
   end
+
+  def test_add_x_to_cached_var1d
+    set_orig = [2, 20, 8]
+    mu_orig = 10
+    sig_orig = 56
+    x = 17
+   
+    assert_equal 51.19, add_x_to_cached_var1d(set_orig, sig_orig, mu_orig, x).round(2) 
+  end
 end
 
--- variance.rb
+++ variance.rb
@@ -2,8 +2,16 @@ module Variance
   include Mean
 
   def var1D(set)
-    u = mean1D(set)
+    mu = mean1D(set)
 
-    set.sum { |n| (n - u)**2 } / set.count
+    set.sum { |xn| (xn - mu)**2 } / set.length
+  end
+
+  def add_x_to_cached_var1d(set_orig, sig_orig, mu_orig, x)
+    n_orig = set_orig.length.to_f # coerce to float, to avoid rounded quotients 
+    n = n_orig + 1    
+    mu = (n_orig / n) * mu_orig + (1.0 / n) * x
+
+    (n_orig / n) * sig_orig + (1 / n) * (x - mu_orig) * (x - mu)
   end
 end
